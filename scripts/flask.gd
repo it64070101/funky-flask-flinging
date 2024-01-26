a96b9a08b448bb8a942dbae28541aa6802355aa4
Thanawat sritho <64070044@kmitl.ac.th>
2024-01-27 01:54:34 +0700

    change path file and add gbl var

--- scripts/flask.gd
+++ scripts/flask.gd
@@ -1,13 +1,17 @@
 extends RigidBody2D
-
+@export var spritelist = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$sprite.set_texture(spritelist[1])
+	name = "1"+str(1)
+	print(name)
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(Gbl.flask[2])
 	if (position.y <= -10):
 		queue_free()
 
---
+++ scripts/gbl.gd
@@ -0,0 +1,2 @@
+extends Node
+var flask = ["0","1","2","3","4","5"] # plase add name
