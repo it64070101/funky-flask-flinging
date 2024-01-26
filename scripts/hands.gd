e5c7369ce1708c3a0b308cf22d8b18b6c9570637
kalamplee <64070101@kmitl.ac.th>
2024-01-26 22:01:46 +0700

    update

---
+++ scripts/hands.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = get_local_mouse_position().normalized().x
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print('left')
+			var flask = load("res://nodes/flask.tscn").instantiate()
+			get_parent().add_child(flask)
+			flask.position = position
