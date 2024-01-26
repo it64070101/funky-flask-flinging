00472813082dc60c03da550fa80ddfb01a8557be
Thanawat sritho <64070044@kmitl.ac.th>
2024-01-26 22:13:00 +0700

    Update flask.gd

--- nodes/flask.gd
+++ nodes/flask.gd
@@ -8,4 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	print(position.y)
+	if (position.y <= -10):
+		queue_free()
