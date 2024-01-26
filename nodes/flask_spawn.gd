72f1f349786fddd05ca013d1ca0f7f83b8487a6c
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2024-01-27 00:29:47 +0700

    Update flask_spawn.gd

--- nodes/flask_spawn.gd
+++ nodes/flask_spawn.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-
+var cooldown = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var flask = load("res://nodes/flask.tscn").instantiate()
-	flask.set_gravity_scale(1)
-	add_child(flask)
+	cooldown = cooldown + 1
+	if(cooldown >= 200):
+		var flask = load("res://nodes/flask.tscn").instantiate()
+		flask.set_gravity_scale(1)
+		add_child(flask)
+		cooldown = 0
+	
 	pass
+	
+func spawn():
+	await get_tree().create_timer(1).timeout
