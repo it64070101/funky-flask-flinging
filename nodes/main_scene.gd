d8da3f4755ad7fdf490b3f46ce2a4ec36ebb050b
kalamplee <64070101@kmitl.ac.th>
2024-01-27 16:50:51 +0700

    set game over

--- nodes/main_scene.gd
+++ nodes/main_scene.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var flaskStore
+var stress
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	flaskStore = 0
+	stress = 0
 	Gbl.gameStart = true
 	pass # Replace with function body.
 
@@ -17,5 +19,11 @@ func addFlaskStore():
 	flaskStore += 1
 	#print(flaskStore)
 	if flaskStore >= 50:
+		$Camera/Control.gameOverScreen()
+		Gbl.gameStart = false
+
+func addStress():
+	stress += 1
+	if stress >= 3:
 		$Camera/Control.gameOverScreen()
 		Gbl.gameStart = false
