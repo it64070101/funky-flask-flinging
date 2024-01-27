2f628edf9a4197f4f5caae3a62490419aa7aa19e
kalamplee <64070101@kmitl.ac.th>
2024-01-27 11:09:59 +0700

    Update gbl.gd

--- scripts/gbl.gd
+++ scripts/gbl.gd
@@ -1,4 +1,16 @@
 extends Node
 var rng = RandomNumberGenerator.new()
-var flask = [0,1,2,3,4,5] # plase add name
+var flask = ["Bomb","Frog","GenderBend","DeAge","Fly","Normal"] # plase add name
 var randomflask = 0
+var money = 0
+var stress = 0
+var combo = 0
+
+func addMoney():
+	money += combo
+
+func addStress():
+	stress += 1
+
+func addCombo():
+	combo += 1
