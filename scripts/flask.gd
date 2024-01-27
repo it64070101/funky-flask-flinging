66b874330382f33965254d59abbfe1ee9c1219af
kalamplee <64070101@kmitl.ac.th>
2024-01-27 14:21:20 +0700

    fix shooting

--- scripts/flask.gd
+++ scripts/flask.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 @export var spritelist = []
 var isInHand
 
+var tag = ""
+
 var isShoot
 # Called when the node enters the scene tree for the first time.
 var random_number
@@ -11,11 +13,13 @@ func _ready():
 	$sprite.set_texture(spritelist[0])
 	isInHand = false
 	isShoot = false
+	tag = ""
 	pass
 
 func _process(_delta):
 	if (position.y <= -10):
 		queue_free()
+		Gbl.lockShoot = false
 	if isInHand:
 		global_position = get_parent().get_node("CollisionShape2D").global_position
 
@@ -23,10 +27,12 @@ func _on_check_customer_body_entered(body):
 	if (body.is_in_group("customer")):
 		if(body.getWantFlask() == random_number):
 			print("yes")
+			Gbl.lockShoot = false
 			body.queue_free()
 			self.queue_free()
 		else:
 			print("No")
+			Gbl.lockShoot = false
 			body.queue_free()
 			self.queue_free()
 		
@@ -35,6 +41,7 @@ func set_random_flask():
 	random_number = int(Gbl.rng.randf_range(0, 6.0))
 	$sprite.set_texture(spritelist[random_number])
 	name = Gbl.flask[random_number]
+	tag = Gbl.flask[random_number]
 
 func inHand():
 	isInHand = true
